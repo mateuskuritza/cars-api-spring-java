f4f9de167f2325ec4629159dcc8314e9d28f6662
mateuskuritza <mateuskuritza@gmail.com>
2023-01-20 12:42:20 -0300

    feat: add some validations with routes to delete and update

--- src/main/java/com/cars/cars/controllers/CarsController.java
+++ src/main/java/com/cars/cars/controllers/CarsController.java
@@ -1,10 +1,14 @@
 package com.cars.cars.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cars.cars.dto.CarDTO;
 import com.cars.cars.model.Car;
 import com.cars.cars.repository.CarRepository;
+
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/cars")
 public class CarsController {
@@ -20,19 +27,39 @@ public class CarsController {
     private CarRepository repository;
 
     @PostMapping
-    public void registerCar(@RequestBody CarDTO car){
+    public void registerCar(@RequestBody @Valid CarDTO car) {
         System.out.println("Modelo: " + car.modelo());
         System.out.println("Fabricante: " + car.fabricante());
         System.out.println("Data de Fabricação: " + car.dataFabricacao());
         System.out.println("Valor: " + car.valor());
         System.out.println("Ano do modelo: " + car.anoModelo());
 
-        
         repository.save(new Car(car));
     }
 
     @GetMapping
-    public List<Car> getCars(){
+    public List<Car> getCars() {
         return repository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCar(@PathVariable Long id, @RequestBody @Valid CarDTO req) {
+        Optional<Car> car = repository.findById(id);
+
+        if (car.isPresent()) {
+            Car carValues = car.get();
+            carValues.setModelo(req.modelo());
+            carValues.setFabricante(req.fabricante());
+            carValues.setDataFabricacao(req.dataFabricacao());
+            carValues.setValor(req.valor());
+            carValues.setAnoModelo(req.anoModelo());
+            repository.save(carValues);
+        }
+
     }
 }
--- src/main/java/com/cars/cars/model/Car.java
+++ src/main/java/com/cars/cars/model/Car.java
@@ -1,5 +1,7 @@
 package com.cars.cars.model;
 
+import java.util.Date;
+
 import com.cars.cars.dto.CarDTO;
 
 import jakarta.persistence.Column;
@@ -15,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Car {
 
-    public Car(CarDTO data){
+    public Car(CarDTO data) {
         this.modelo = data.modelo();
         this.fabricante = data.fabricante();
         this.dataFabricacao = data.dataFabricacao();
@@ -33,8 +35,8 @@ public class Car {
     @Column(length = 50, nullable = false)
     private String fabricante;
 
-    @Column(length = 10, nullable = false)
-    private String dataFabricacao;
+    @Column(nullable = false)
+    private Date dataFabricacao;
 
     @Column(length = 20, nullable = false)
     private double valor;
@@ -42,5 +44,3 @@ public class Car {
     @Column(length = 10, nullable = false)
     private int anoModelo;
 }
-
-
